023a661ef1a7b97d6cdab928047c1b0faef049db
Tsering Tsering <dtsering2@gmail.com>
2022-09-02 15:44:09 -0400

    commit tree 368d39f0af109ecab4a6acc501f692f2b71d4771

---
+++ lib/song.rb
@@ -0,0 +1,51 @@
+class Song
+    @@count = 0
+    @@artists =[]
+    @@genres = []
+
+    attr_reader :name, :artist, :genre
+
+    def initialize(name, artist, genre)
+        @name = name
+        @artist = artist
+        @genre = genre
+        @@count += 1
+        @@artists << artist
+        @@genres << genre
+    end
+
+    def self.count
+        @@count
+    end
+
+    def self.artists
+        @@artists.uniq
+    end
+
+    def self.genres
+        @@genres.uniq
+    end
+
+    def self.genre_count
+        genre_hash = {}
+
+        @@genres.each do |genre|
+            if genre_hash[genre] == 1
+                genre_hash[genre] +=1
+            else
+                genre_hash[genre] = 1
+            end
+        end
+        genre_hash
+    end
+
+    def self.artist_count
+        @@artists.tally
+    end
+
+
+
+
+
+
+end
